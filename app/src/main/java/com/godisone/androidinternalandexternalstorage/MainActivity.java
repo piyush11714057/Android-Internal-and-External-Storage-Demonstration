e2632fcd47ef9a26cee53b51b4079967b70c407b
piyush11714057 <piyush.11714057@lpu.in>
2020-04-19 10:55:47 +0530

    Splash Screen and Main Activity layout

--- app/src/main/java/com/godisone/androidinternalandexternalstorage/MainActivity.java
+++ app/src/main/java/com/godisone/androidinternalandexternalstorage/MainActivity.java
@@ -1,15 +1,23 @@
 package com.godisone.androidinternalandexternalstorage;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button b1,b2,b3;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b1=findViewById(R.id.btn_definitions);
+        b2=findViewById(R.id.btn_internal_Storage);
+        b3=findViewById(R.id.btn_external_storage);
+
 
     }
 }
---
+++ app/src/main/java/com/godisone/androidinternalandexternalstorage/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.godisone.androidinternalandexternalstorage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent a = new Intent(SplashScreen.this,MainActivity.class);
+                    startActivity(a);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+
+}
+
